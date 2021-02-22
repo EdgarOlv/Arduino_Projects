f7d52dc245c2de76cf6394a4e537987804a3cae7
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-02-22 20:49:04 -0300

    Botao

---
+++ Botao/Botao.ino
@@ -0,0 +1,33 @@
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+boolean troca = false;
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() 
+{
+   buttonState = digitalRead(buttonPin);
+    if(buttonState== HIGH & troca == false){
+    troca = true;
+    delay(150);
+    }else{
+      delay(150);
+    if(buttonState== HIGH & troca == true)
+      troca = false;
+      
+    }
+    
+  if (troca == HIGH) {
+  digitalWrite(ledPin, HIGH);
+  
+  } 
+  else{
+    digitalWrite(ledPin, LOW);
+  }
+}

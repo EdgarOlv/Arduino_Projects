cd3749526cc605489ac1b1e0f9927ba547b4fd1a
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-02-25 19:11:01 -0300

    TecladoCapacitivo

---
+++ Teclado-Capacitivo/Capacitivo_sensor/Capacitivo_sensor.ino
@@ -0,0 +1,21 @@
+#include<CapacitiveSensor.h>
+
+CapacitiveSensor sensor = CapacitiveSensor(6,7);
+
+void setup()
+{
+  pinMode(13,OUTPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  long lectura = sensor.capacitiveSensor(30);
+  Serial.println(lectura);
+  if(lectura>1)
+    digitalWrite(13,HIGH);
+  else
+  digitalWrite(13,LOW);
+
+    delay(10);
+}
+

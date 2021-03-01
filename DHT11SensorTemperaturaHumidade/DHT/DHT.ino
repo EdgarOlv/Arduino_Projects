923bc7d5ce6622de05fa0626d3d54582518d8651
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-03-01 08:17:44 -0300

    DHT 11

---
+++ DHT11SensorTemperaturaHumidade/DHT/DHT.ino
@@ -0,0 +1,21 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 4
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int chk = DHT.read11(DHT11_PIN);
+  if(DHT.temperature<500 and DHT.temperature>-20){
+  Serial.print("Temperature = ");
+  Serial.println(DHT.temperature);
+  Serial.print("Humidity = ");
+  Serial.println(DHT.humidity);
+  }
+  delay(500);
+}

1c6deb3cde6957de663c2dd46f2e6e9bafd4dccc
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-03-05 10:59:16 -0300

    Direita Esquerda LCD

---
+++ DisplayLCD/Correr_para_esquerda_e_direita/Correr_para_esquerda_e_direita.ino
@@ -0,0 +1,28 @@
+ 
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+//Escolhe onde posicionar->  lcd.setCursor(0, 0);
+
+LiquidCrystal_I2C lcd(0x3f,16,2);  // Criando um LCD de 16x2 no endereço 0x20
+
+void setup()
+{
+  lcd.init();                 // Inicializando o LCD
+  lcd.backlight();            // Ligando o BackLight do LCD
+
+  lcd.print(" Funcionando!!!");
+    for (int i = 0; i < 14; i++) {
+    lcd.scrollDisplayLeft();
+    delay(600);
+  }
+  for (int i = 0; i < 20; i++) {
+    lcd.scrollDisplayRight();
+    delay(600);
+  }
+}
+
+void loop()
+{
+ 
+}

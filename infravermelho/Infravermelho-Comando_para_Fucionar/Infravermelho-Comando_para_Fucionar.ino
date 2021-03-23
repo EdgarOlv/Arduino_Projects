a5d3d2ab470bdaa509aa9dd4fb4ec858f92032a0
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-03-23 11:43:31 -0300

    Comando

---
+++ infravermelho/Infravermelho-Comando_para_Fucionar/Infravermelho-Comando_para_Fucionar.ino
@@ -0,0 +1,4 @@
+//liga led vermelho    
+   if ((codigo_recebido.value == 0x39C6D02F) || (codigo_recebido.value == 0xFF7A85)){// verifica se foi pressionado o numero 3 dos controles    
+   digitalWrite(ledvermelho, HIGH);   
+   delay(500);    

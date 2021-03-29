05813fb77a0a7cd15983eab31a6baeadad0e60c4
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-03-29 09:28:10 -0300

    LDR

---
+++ LDR/LDR.ino
@@ -0,0 +1,25 @@
+ 
+int ledPin = 13; //Led no pino 7
+int ldrPin = 10; //LDR no pino analígico 8
+int ldrValor = 0; //Valor lido do LDR
+ 
+void setup() {
+ pinMode(ledPin,OUTPUT); //define a porta 7 como saída
+ Serial.begin(9600); //Inicia a comunicação serial
+}
+ 
+void loop() {
+ ///ler o valor do LDR
+ 
+ ldrValor = analogRead(ldrPin); //O valor lido será entre 0 e 1023
+ 
+ //se o valor lido for maior que 500, liga o led
+ if (ldrValor>= 1000) 
+   digitalWrite(ledPin,HIGH);
+ // senão, apaga o led
+ else digitalWrite(ledPin,LOW);
+ 
+ //imprime o valor lido do LDR no monitor serial
+ Serial.println(ldrValor);
+ delay(100);
+}

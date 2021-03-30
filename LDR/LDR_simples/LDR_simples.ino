83c213c947f4537d5ac73cf8dcd0a80022cdc577
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-03-30 07:55:43 -0300

    LDR Simple

---
+++ LDR/LDR_simples/LDR_simples.ino
@@ -0,0 +1,17 @@
+
+int ldr = 0; 
+int ldrValor = 0; //Valor lido do LDR
+
+void setup() 
+{
+ Serial.begin(9600);
+}
+
+void loop() 
+{
+   ///ler o valor do LDR
+   ldrValor = analogRead(ldr); //O valor lido será entre 0 e 1023
+   Serial.println(ldrValor);
+   delay(100);
+
+}

0f8a65dc40877385bcd53ca5102520172e36b347
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-04-01 13:34:41 -0300

    Led Serial

---
+++ LedSerial/LedSerial.ino
@@ -0,0 +1,25 @@
+void setup() 
+{
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Input 1 to Turn LED on and 2 to off");
+}
+
+void loop()
+{
+  if (Serial.available())
+  {
+    int state = Serial.parseInt();
+    if (state == 1)
+    {
+     digitalWrite(13, HIGH);
+     Serial.println("Command completed LED turned ON");
+    }
+    if (state == 2)
+    {
+     digitalWrite(13, LOW);
+     Serial.println("Command completed LED turned OFF");
+    }  
+  }
+}

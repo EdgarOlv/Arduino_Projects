15bb619525933618129b347547332a4deedb7949
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-04-14 07:33:13 -0300

    Motor de Passo

---
+++ motor_de_passo_ponteH/motor_de_passo_ponteH.ino
@@ -0,0 +1,51 @@
+  
+// Programa : Ponte H L298N e motor de passo
+// Autor : Arduino e Cia
+
+// Conexoes modulo - Arduino
+int IN1 = 8;
+int IN2 = 9;
+int IN3 = 10;
+int IN4 = 11;
+
+void setup()
+{
+  // Define os pinos como saida
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+}
+
+int tempo = 10;
+
+void loop()
+{
+  // Passo 1
+  digitalWrite(IN1, 1);
+  digitalWrite(IN2, 0);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
+  delay(tempo);  
+
+  // Passo 2
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
+  digitalWrite(IN3, 0);
+  digitalWrite(IN4, 1);
+  delay(tempo);
+
+  // Passo 3
+  digitalWrite(IN1, 0);
+  digitalWrite(IN2, 1);
+  digitalWrite(IN3, 1);
+  digitalWrite(IN4, 0);
+  delay(tempo);
+
+  // Passo 4
+  digitalWrite(IN1, 1);
+  digitalWrite(IN2, 0);
+  digitalWrite(IN3, 1);
+  digitalWrite(IN4, 0);
+  delay(tempo);
+}

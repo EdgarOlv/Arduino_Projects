1145f1b516df43b6434dde4683d146d394687fbd
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-04-21 08:47:41 -0300

    Motor Shield

---
+++ MotorShield/MotorShield/MotorShield.ino
@@ -0,0 +1,29 @@
+//Programa : Teste de motor DC12V com motor shield ponte H
+//Autor : Equipe FILIPEFLOP
+
+#include <AFMotor.h>
+
+AF_DCMotor motor(1); //Seleciona o motor 1
+
+void setup()
+{}
+
+void loop()
+{
+motor.setSpeed(255); //Define a velocidade maxima
+motor.run(FORWARD); //Gira o motor sentido horario
+
+delay(5000);
+motor.setSpeed(0);
+motor.run(RELEASE); //Desliga o motor
+
+delay(5000);
+motor.setSpeed(100); //Define velocidade baixa
+motor.run(BACKWARD); //Gira o motor sentido anti-horario
+
+delay(5000);
+motor.setSpeed(0);
+motor.run(RELEASE); //Desliga o motor
+
+delay(5000); //Aguarda 5 segundos e repete o processo
+}

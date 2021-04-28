aa66f23134a770c7b3cb6ecbae96bebec9cc0e36
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-04-28 08:02:45 -0300

    TCRT5000

---
+++ Sensor óptico reflexivo TCRT5000/Ligar_led_TCRT5000/Ligar_led_TCRT5000.ino
@@ -0,0 +1,36 @@
+/*********************************
+Autor: Fernando Krein Pinheiro
+Data: 25/01/2012
+Linguagem: C (Wiring)
+========= IMPORTANTE ===========
+O código esta livre para usar,
+citar e compartilhar desde que
+mantida sua fonte e seu autor.
+Obrigado.
+********************************/
+ 
+int ledPin = 13;
+int inPin = 10;
+int valor = 0;
+ 
+void setup()
+{
+   Serial.begin(9600);
+   pinMode(ledPin, OUTPUT);
+   pinMode(inPin, INPUT);
+}
+ 
+void loop()
+{
+    valor = digitalRead(inPin);
+ 
+    if (valor == HIGH)
+    {
+           digitalWrite(ledPin, HIGH);
+       Serial.write("Preto");
+    } else
+         {
+               digitalWrite(ledPin, LOW);
+           Serial.write("Branco");
+        }
+}

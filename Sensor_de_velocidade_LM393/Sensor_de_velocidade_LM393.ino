0a48a98d1b95a8e82b5b16998d8c0af17a2441d1
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-04-28 08:04:07 -0300

    LM393

---
+++ Sensor_de_velocidade_LM393/Sensor_de_velocidade_LM393.ino
@@ -0,0 +1,49 @@
+//Programa: Sensor de velocidade Arduino LM393
+//Autor: Arduino e Cia
+
+//Pino ligado ao pino D0 do sensor
+int pino_D0 = 2;
+int rpm;
+volatile byte pulsos;
+unsigned long timeold;
+
+//Altere o numero abaixo de acordo com o seu disco encoder
+unsigned int pulsos_por_volta = 20;
+
+void contador()
+{
+  //Incrementa contador
+  pulsos++;
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  //Pino do sensor como entrada
+  pinMode(pino_D0, INPUT);
+  //Interrupcao 0 - pino digital 2
+  //Aciona o contador a cada pulso
+  attachInterrupt(0, contador, FALLING);
+  pulsos = 0;
+  rpm = 0;
+  timeold = 0;
+}
+
+void loop()
+{
+  //Atualiza contador a cada segundo
+  if (millis() - timeold >= 300)
+  {
+    //Desabilita interrupcao durante o calculo
+    detachInterrupt(0);
+    rpm = (60 * 1000 / pulsos_por_volta ) / (millis() - timeold) * pulsos;
+    timeold = millis();
+    pulsos = 0;
+
+    //Mostra o valor de RPM no serial monitor
+    Serial.print("RPM = ");
+    Serial.println(rpm, DEC);
+    //Habilita interrupcao
+    attachInterrupt(0, contador, FALLING);
+  }
+}

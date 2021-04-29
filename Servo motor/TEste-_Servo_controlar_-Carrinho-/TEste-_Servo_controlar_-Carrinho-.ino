1bf860f4c68725a18b07093fbff58e3ea95c7358
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-04-29 08:58:55 -0300

    Controle Carro

---
+++ Servo motor/TEste-_Servo_controlar_-Carrinho-/TEste-_Servo_controlar_-Carrinho-.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>
+Servo servomotorD;
+Servo servomotorE
+
+void setup() {
+  ServomotorD.attach(5);
+  delay(100);
+  servomotorE.attach(6);
+}
+
+void loop() {
+  giraRodaDireitaFrente();
+  servomotorE.write(0);
+  delay(2000);
+  }
+  void giraRodaDireitaFrente(){
+    servomotorD.write(180);
+  }
+  void giraRodaEsquerdaFrente(){
+    servomotorE.write(0);
+  }
+

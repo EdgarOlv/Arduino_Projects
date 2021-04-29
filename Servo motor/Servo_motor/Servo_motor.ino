df593855eefb9eec2bcd36af8715d3f677dd5b1f
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-04-29 08:58:37 -0300

    Codigo

---
+++ Servo motor/Servo_motor/Servo_motor.ino
@@ -0,0 +1,28 @@
+#include span style="color: #cc6600;">Servo.h>
+
+
+Servo motor;                           
+
+
+
+ void setup()
+
+{
+
+   motor.attach(3); 
+
+}
+
+void loop () 
+
+{ 
+
+  motor.write(45); 
+
+  delay(1000); 
+
+  motor.write(125); 
+
+  delay(1000); 
+
+}

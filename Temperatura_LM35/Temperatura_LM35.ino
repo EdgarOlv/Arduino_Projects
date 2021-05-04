e15a97219321cba524a65f121ad777af8409f141
Edgar Oliveira Cardoso <edgaroliveira1258@gmail.com>
2021-05-04 13:26:52 -0300

    LM35

---
+++ Temperatura_LM35/Temperatura_LM35.ino
@@ -0,0 +1,17 @@
+int pinoSensor = 0;
+
+int valorLido = 0;
+float temperatura = 0;
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    valorLido = analogRead(pinoSensor);
+    temperatura = (valorLido * 0.00488);
+    temperatura = temperatura * 100;
+    Serial.print("Temperatura actual: ");
+    Serial.println(temperatura);
+    delay(1000);
+}
